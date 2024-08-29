bad88e4ab517463020043b54338687cb0e992bcc
hr-nmm <himanshu_code@hotmail.com>
2024-08-30 03:20:38 +0530

    add lint

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 // imports
-const config = require("./utils/config");
-const express = require("express");
-const app = express();
-const logger = require("./utils/logger");
-const blogsRouter = require("./controllers/blogs");
-const mongoose = require("mongoose");
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
 
 // connect to DB
-mongoose.set("strictQuery", false);
-logger.info("connecting to ", config.MONGODB_URI);
-mongoose.connect(config.MONGODB_URI).then((result) => {
-  logger.info("connected to MongoDB");
-});
+mongoose.set('strictQuery', false)
+logger.info('connecting to ', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI).then(() => {
+  logger.info('connected to MongoDB')
+})
 
 // use middlewares
-app.use(express.json());
-app.use("/api/blogs", blogsRouter);
-module.exports = app;
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
